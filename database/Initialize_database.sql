038bc3d42bbb0681ddfa549199a77d81b41dc841
Juan2k-1 <jadova16@gmail.com>
2023-09-16 20:06:17 +0200

    New classes added in the generation database script

--- database/Initialize_database.sql
+++ database/Initialize_database.sql
@@ -51,6 +51,85 @@ CREATE TABLE RECIPE_ELABORATION_STEP (
     FOREIGN KEY (id_recipe) references RECIPE
 );
 
+-- Crea la tabla de datos de usuario de fitness
+CREATE TABLE SALUHUD_USER_FITNESS_DATA (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    weight real,
+    height real,
+    biological_sex BIOLOGICAL_SEX,
+    age smallint,
+    body_composition text,
+    recommended_daily_water_liters smallint,
+    recommended_sleep_hours smallint,
+    recommended_daily_steps smallint,
+    daily_kilocalories_objective smallint,
+    body_mass_index text
+);
+
+-- Crea la tabla de datos personales del usuario
+CREATE TABLE SALUHUD_USER_PERSONAL_DATA (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_fitness_data_id bigint NOT NULL,
+    name text NOT NULL,
+    surname text,
+    phone_number text UNIQUE,
+    FOREIGN KEY (user_fitness_data_id) REFERENCES SALUHUD_USER_FITNESS_DATA(id)
+);
+
+-- Crea la tabla del historial de sueño
+CREATE TABLE SLEEP_HISTORICAL (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES SALUHUD_USER_PERSONAL_DATA(id)
+);
+
+-- Crea la tabla de entrada del historial de sueño
+CREATE TABLE SLEEP_HISTORICAL_ENTRY (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    sleep_historical_id bigint NOT NULL,
+    date date NOT NULL,
+    hours_slept smallint NOT NULL,
+    minutes_slept smallint NOT NULL,
+    sleep_evaluation HISTORICAL_EVALUATION NOT NULL,
+    FOREIGN KEY (sleep_historical_id) REFERENCES SLEEP_HISTORICAL(id)
+);
+
+-- Crea la tabla del historial de pasos diarios
+CREATE TABLE DAILY_STEPS_HISTORICAL (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES SALUHUD_USER_PERSONAL_DATA(id)
+);
+
+-- Crea la tabla de entrada del historial de pasos diarios
+CREATE TABLE DAILY_STEPS_HISTORICAL_ENTRY (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    daily_steps_historical_id bigint NOT NULL,
+    date date NOT NULL,
+    done_steps smallint NOT NULL,
+    kilo_calories_burned real NOT NULL,
+    steps_evaluation HISTORICAL_EVALUATION NOT NULL,
+    FOREIGN KEY (daily_steps_historical_id) REFERENCES DAILY_STEPS_HISTORICAL(id)
+);
+
+-- Crea la tabla del historial de peso
+CREATE TABLE WEIGHT_HISTORICAL (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id bigint NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES SALUHUD_USER_PERSONAL_DATA(id)
+);
+
+-- Crea la tabla de entrada del historial de peso
+CREATE TABLE WEIGHT_HISTORICAL_ENTRY ( 
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    weight_historical_id bigint NOT NULL,
+    date date NOT NULL,
+    weight_entry real NOT NULL,
+    height_entry real NOT NULL,
+    kilo_calories_objective_entry real NOT NULL,
+    FOREIGN KEY (weight_historical_id) REFERENCES WEIGHT_HISTORICAL(id)
+);
+
 INSERT INTO ALLERGENIC(name) VALUES ('GLUTEN');
 INSERT INTO ALLERGENIC(name) VALUES ('CRUSTACEAN');
 INSERT INTO ALLERGENIC(name) VALUES ('EGG');

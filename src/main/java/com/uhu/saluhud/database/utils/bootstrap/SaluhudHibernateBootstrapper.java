a7677a27a0319e8dc83eedc7493074ac72cc5006
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-09-22 01:45:19 +0200

    Implemented methods to dynamically add Entity classes to Hibernate

--- src/main/java/com/uhu/saluhud/database/utils/bootstrap/SaluhudHibernateBootstrapper.java
+++ src/main/java/com/uhu/saluhud/database/utils/bootstrap/SaluhudHibernateBootstrapper.java
@@ -1,8 +1,10 @@
 package com.uhu.saluhud.database.utils.bootstrap;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +19,12 @@ import org.hibernate.boot.MetadataSources;
 public abstract class SaluhudHibernateBootstrapper
 {    
     private SessionFactory sessionFactory;
+    
+    private static final String DATABASE_MODEL_CLASSES_BASE_DIRECTORY = 
+            "src/main/java/com/uhu/saluhud/database/utils/models";
+    
+    private static final String DATABASE_MODEL_CLASSES_BASE_PACKAGE = 
+            "com.uhu.saluhud.database.utils.models";
     
     public abstract void closeSessionFactory();
     
@@ -34,18 +42,23 @@ public abstract class SaluhudHibernateBootstrapper
      */
     protected void addAllAnnotatedClasses(MetadataSources metadataSources)
     {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream("com.uhu.saluhud.database.utils.models".replaceAll("[.]", "/"));
+        Set<String> databaseModelPackages = getDatabaseModelPackages();
         
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        Set<Class> databaseModelClasses = new HashSet<>();
         
-        Set<Class> modelPackageClasses = reader.lines().filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, "com.uhu.saluhud.database.utils.models"))
-                .collect(Collectors.toSet());
-        
-        for (Class modelPackageClass : modelPackageClasses)
+        for (String databaseModelPackage : databaseModelPackages)
         {
-            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Model Package Classes: " + modelPackageClass);
+            Set<Class> packageModelClasses = getPackageDatabaseModelClasses(databaseModelPackage);
+            
+            databaseModelClasses.addAll(packageModelClasses);
+        }
+        
+        for (Class databaseModelClass : databaseModelClasses)
+        {
+            if(databaseModelClass.isAnnotationPresent(javax.persistence.Entity.class))
+            {
+                metadataSources.addAnnotatedClass(databaseModelClass);
+            }
         }
     }
     
@@ -61,6 +74,42 @@ public abstract class SaluhudHibernateBootstrapper
         }
         
         return null;
+    }
+    
+    public Set<String> getDatabaseModelPackages()
+    {
+        Set<String> databaseModelPackages = new HashSet<>();
+        
+        databaseModelPackages.add(DATABASE_MODEL_CLASSES_BASE_PACKAGE);
+        
+        File modelsDirectory = new File(DATABASE_MODEL_CLASSES_BASE_DIRECTORY);
+        
+        File[] modelsDirectoryFiles = modelsDirectory.listFiles();
+        
+        for (File modelDirectoryFile : modelsDirectoryFiles)
+        {
+            if(modelDirectoryFile.isDirectory())
+            {
+                databaseModelPackages.add(modelDirectoryFile.getPath().replaceAll("[/\\\\]", ".")
+                .replaceFirst("src.main.java.", ""));
+            }
+        }
+        
+        return databaseModelPackages;
+    }
+    
+    private Set<Class> getPackageDatabaseModelClasses(String packageName)
+    {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        
+        Set<Class> modelPackageClasses = reader.lines().filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toSet());
+        
+        return modelPackageClasses;
     }
 
     public SessionFactory getSessionFactory()
